686bf1e5a37e67bb1026db3804056d5f680bf1e7
Iuri Borja <95659045+IuriBorja@users.noreply.github.com>
2023-08-29 18:26:48 +0100

    Initial commit

---
+++ src/main/java/com/example/menu/controller/FoodController.java
@@ -0,0 +1,23 @@
+package com.example.menu.controller;
+
+
+import com.example.menu.food.Food;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("food")
+public class FoodController {
+
+    @GetMapping
+    public void getAll() {
+
+        Food food
+
+    }
+
+
+
+
+}
